b41a3e0822c85117c85efa187fa67dd6a91c6d88
asad2733 <92863725+asad2733@users.noreply.github.com>
2022-08-05 18:51:01 +0530

    Added makedocker.sh

---
+++ makedocker.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/asad2733/spring-petclinic-docker.git
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 615441698862.dkr.ecr.us-east-1.amazonaws.com
+docker build -t petclinic .
+docker tag petclinic:latest 615441698862.dkr.ecr.us-east-1.amazonaws.com/petclinic:latest
+docker push 615441698862.dkr.ecr.us-east-1.amazonaws.com/petclinic:latest
